5f5dadd68767975831a119ac6b721fc412153fb1
Spetsnaz5 <167165443+Spetsnaz5@users.noreply.github.com>
2024-07-19 16:34:32 +0800

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,17 +1,17 @@
 import random
 
-#氣泡排序
-x = random.sample(range(1, 100), 10)
+# 從1-100中不重複隨機產生9組數字
+A = random.sample(range(1,100),9)
 
-print(x)
+print(A)
 
-for num in range(len(x)):
-    for index in range(len(x) - num):
-        if index+1 >= len(x):
-            break
-        if x[index] > x[index+1]:
-            tmp = x[index+1]
-            x[index+1] = x[index]
-            x[index] = tmp
+for i in range(len(A)-1,0,-1):
+    for j in range(i):
+        if A[j] > A[j+1]:
+            #交換數字
+            A[j],A[j+1] = A[j+1],A[j]
+    print("氣泡排序外層迴圈執行第", 9-i,"次")
 
-print(x)
+    for item in A:
+        print(item,' ',end="")
+    print()
