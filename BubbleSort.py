d4cb1383f34f177aff434a5331d937467d51a42e
Spetsnaz5 <167165443+Spetsnaz5@users.noreply.github.com>
2024-07-19 11:43:04 +0800

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,1 +1,16 @@
+import random
 
+x = random.sample(range(1, 100), 10)
+
+print(x)
+
+for num in range(len(x)):
+    for index in range(len(x) - num):
+        if index+1 >= len(x):
+            break
+        if x[index] > x[index+1]:
+            temp = x[index + 1]
+            x[index + 1] = x[index]
+            x[index] = temp
+
+print(x)
